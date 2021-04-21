261659ec5672831c9e4dfc3fd53afcd707010dc7
Kamil Gierada <vlaska8888@gmail.com>
2021-04-22 00:23:41 +0200

    Optimizing

--- DLA/utils.pyx
+++ DLA/utils.pyx
@@ -4,28 +4,39 @@
 
 import numpy as np
 cimport numpy as np
+from cython.view cimport array as cvarray
 
 ctypedef np.double_t DFLOAT
 ctypedef np.npy_bool DBOOL
 
 
-def squared_distance(np.ndarray[DFLOAT, ndim=2] v):
-    return np.sum(v * v, axis=1)
+cpdef double[:] squared_distance(double[:, :] v):
+    cdef Py_ssize_t i, size = v.shape[0]
+    # cdef double[:] out = np.empty(size, dtype=np.double)
+    cdef double[:] out = cvarray(shape=(size, ), itemsize=sizeof(double), format="i")
+    for i in range(size):
+        out[i] = v[i, 0] * v[i, 0] + v[i, 1] * v[i, 1]
+
+    return out
 
 
-def does_collide(np.ndarray[DFLOAT, ndim=2] pos, np.ndarray[DFLOAT, ndim=1] point, float radius):
-    cdef np.ndarray[DFLOAT, ndim=2] diffs
-    cdef np.ndarray[DBOOL, ndim=1] t
-    cdef np.ndarray[DFLOAT, ndim=1] r
-    cdef np.ndarray[DFLOAT, ndim=2] stuck_pos
+def does_collide(double[:, :] pos, double[:, :] point, float radius):
+    #cdef double[:, :] diffs
+    #cdef bool[:] t
+    #cdef double[:] r
+    cdef double[:, :] stuck_pos
 
-    diffs = np.abs(pos - point)
-    t = squared_distance(diffs) < (4 * radius * radius)
-    r = diffs[t].compressed()
-    stuck_pos = r.reshape((r.shape[0] // 2, 2))
+    cdef Py_ssize_t pos_size = pos.shape[0]
+    cdef Py_ssize_t i
+    # for i in range(pos_size):
+        
+    #diffs = np.abs(pos - point)
+    #t = squared_distance(diffs) < (4 * radius * radius)
+    #r = diffs[t].compressed()
+    #stuck_pos = r.reshape((r.shape[0] // 2, 2))
 
-    if stuck_pos.any():
-        return True
+    #if stuck_pos.any():
+    #    return True
     return False
 
 
