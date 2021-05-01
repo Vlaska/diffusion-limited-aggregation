e07aebad6c1d9ca8a5b0022c7753c601b2387d0a
Kamil Gierada <vlaska8888@gmail.com>
2021-05-01 19:43:55 +0200

    Add type hints to utls.pyx/utils.py

---
+++ DLA/utils.pyi
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import List, Tuple
+
+import numpy as np
+
+
+def squared_distance(v: np.ndarray) -> np.ndarray: ...
+
+
+def does_collide(pos: np.ndarray, point: np.ndarray,
+                 radius: float) -> bool: ...
+
+
+def test_collisions(pos: np.ndarray, point: np.ndarray,
+                    radius: float) -> List[bool]: ...
+
+
+def in_square(a: np.ndarray, b: np.ndarray, c: np.ndarray) -> bool: ...
+
+
+def one_subchunk_coords(start: np.ndarray, size: float,
+                        idx: int) -> Tuple[float, float]: ...
+
+
+def subchunk_coords(start: np.ndarray,
+                    size: float) -> List[Tuple[float, float]]: ...
+
+
+def circle_in_subchunks(start: np.ndarray, circle_pos: np.ndarray,
+                        suze: float, raduis: float) -> List[int]: ...
