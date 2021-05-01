1b796fd72e1197ce6b556dd4bbf91ab4a81c0f93
Kamil Gierada <vlaska8888@gmail.com>
2021-05-01 19:30:28 +0200

    Fixed cliping of points with window border; optimized clip check

--- DLA/walker/walker_population.py
+++ DLA/walker/walker_population.py
@@ -1,30 +1,33 @@
 from __future__ import annotations
 
-from DLA import Vec2
 from typing import Final, cast
 
 import numpy as np
+from DLA import Vec, Vec2, config
 from numpy import ma
 
-from DLA import Vec, config
-from .utils import random_in_range
 from .stuck_walkers import StuckWalkers
+from .utils import random_in_range
 from .walker import Walker
 
-
-WINDOW_SIZE: Final[Vec2] = cast(Vec2, (config['window_size'], ) * 2)
+WINDOW_SIZE: Final[float] = config['window_size']
+RADIUS: Final[float] = config['point_radius']
+BORDER_U_L: Final[float] = RADIUS
+BORDER_D_R: Final[float] = config['window_size'] - RADIUS
 
 
 class WalkerPopulation(Walker):
     def __init__(self, size: int) -> None:
         super().__init__(size)
-        self.pos[:, 0] = random_in_range(0, WINDOW_SIZE[0], (size,))
-        self.pos[:, 1] = random_in_range(0, WINDOW_SIZE[1], (size,))
+        self.pos[:, :] = random_in_range(0, WINDOW_SIZE, (size, 2))
 
     def walk(self) -> None:
-        self.pos = self.pos + random_in_range(-5, 5, (self.size, 2))
-        self.pos[:, 0] = np.clip(self.pos[:, 0], 0, WINDOW_SIZE[0])
-        self.pos[:, 1] = np.clip(self.pos[:, 1], 0, WINDOW_SIZE[1])
+        np.clip(
+            self.pos + random_in_range(-5, 5, (self.size, 2)),
+            BORDER_U_L,
+            BORDER_D_R,
+            out=self.pos
+        )
 
     @staticmethod
     def pass_to_stuck(point: Vec, other: StuckWalkers) -> None:
