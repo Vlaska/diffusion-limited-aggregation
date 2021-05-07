da6d5d6fd2383f8a5b04b86578b60e166bdd657e
Kamil Gierada <vlaska8888@gmail.com>
2021-05-07 22:26:59 +0200

    Close #1: regenerate walking population

--- DLA/walker/walker_population.py
+++ DLA/walker/walker_population.py
@@ -16,6 +16,7 @@ WINDOW_SIZE: Final[float] = config['window_size']
 RADIUS: Final[float] = config['point_radius']
 BORDER_U_L: Final[float] = RADIUS
 BORDER_D_R: Final[float] = config['window_size'] - RADIUS
+REGENERATE_AFTER: Final[int] = config['regen_after_updates']
 
 
 class WalkerPopulation(Walker):
@@ -23,6 +24,7 @@ class WalkerPopulation(Walker):
         super().__init__(size)
         self.pos[:, :] = random_in_range(0, WINDOW_SIZE, (size, 2))
         self.last_step: np.ndarray
+        self.last_regen = 0
 
     def walk(self) -> None:
         self.last_step = random_in_range(-5, 5, (self.size, 2))
@@ -76,3 +78,12 @@ class WalkerPopulation(Walker):
     def is_stuck(self, other: StuckWalkers) -> None:
         while self._is_stuck(other):
             pass
+
+        self.regenerate_population()
+
+    def regenerate_population(self):
+        if self.last_regen >= REGENERATE_AFTER:
+            self.pos = self.pos[~np.isnan(self.pos[:, 0])]
+            self.last_regen = -1
+            self.size = self.pos.shape[0]
+        self.last_regen += 1
