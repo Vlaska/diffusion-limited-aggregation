8df9c31192fc616dadd88e094bb2cf86d3225eec
Kamil Gierada <vlaska8888@gmail.com>
2021-05-25 18:51:00 +0200

    Start simulation process

--- DLA/server/worker.py
+++ DLA/server/worker.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+import sys
 import pickle
 
 
@@ -13,5 +14,18 @@ class Worker:
         writer.write(pickle.dumps(self))
         await writer.drain()
 
-    async def start_work(self) -> None:
+    async def _run_work(self) -> str:
+        proc = await asyncio.create_subprocess_exec(
+            sys.executable, "-m", "DLA", "simulate", "-",
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE
+        )
+        return (
+            await proc.communicate(self.config.encode('utf-8'))
+        )[0].decode('utf-8').strip()
+
+    async def start_work(self) -> bool:
+        pass
+
+    async def return_results(self, writer: asyncio.StreamWriter) -> None:
         pass
