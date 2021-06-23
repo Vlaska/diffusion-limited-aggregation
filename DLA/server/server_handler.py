f5e707fd032129442a1bc1cc3f451a31b2b05eae
Kamil Gierada <vlaska8888@gmail.com>
2021-06-23 22:06:15 +0200

    Fix: memory value saved with wrong key for client

--- DLA/server/server_handler.py
+++ DLA/server/server_handler.py
@@ -32,7 +32,7 @@ class Handler:
 
     async def send_config(self, conn_data: ConnData) -> None:
         config = CONFIG_TEMPLATE.copy()
-        config['beta'] = conn_data.beta
+        config['memory'] = conn_data.beta
 
         config_dump = cast(str, yaml.dump(config)).encode('utf-8')
         await conn_data.write(
